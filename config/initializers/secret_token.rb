a286528408566339f32be71597fafa7207e13f0d
dotob <sk@dotob.de>
2012-09-08 15:14:10 +0200

    initial commit
    nearly anything setup for starting the real work

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Lgtrack::Application.config.secret_token = '5088fc0e60b8aa72265ba3832f78f1e08bace8bee4657da71ab128acc5ee1291cd46d7e386f762019eee4387c3dae78116877cdc9c1660e768082bd6684b6608'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,8 @@
+# Be sure to restart your server when you modify this file.
+
+Lgtrack::Application.config.session_store :cookie_store, key: '_lgtrack_session'
+
+# Use the database for sessions instead of the cookie-based default,
+# which shouldn't be used to store highly confidential information
+# (create the session table with "rails generate session_migration")
+# Lgtrack::Application.config.session_store :active_record_store
